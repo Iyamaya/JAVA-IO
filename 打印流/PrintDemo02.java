ab1bd379584ebfc7c9a4f07f46a17c614375fdb2
Iyamaya <44797261+Iyamaya@users.noreply.github.com>
2020-07-20 08:11:17 -0500

    Add files via upload

--- 打印流/PrintDemo02.java
+++ 打印流/PrintDemo02.java
@@ -1,12 +1,14 @@
 import java.io.* ;
-public class PrintDemo01{
+public class PrintDemo02{
 	public static void main(String arg[]) throws Exception{
 		PrintStream ps = null ;		// 声明打印流对象
 		// 如果现在是使用FileOuputStream实例化，意味着所有的输出是向文件之中
 		ps = new PrintStream(new FileOutputStream(new File("d:" + File.separator + "test.txt"))) ;
-		ps.print("hello ") ;
-		ps.println("world!!!") ;
-		ps.print("1 + 1 = " + 2) ;
+		String name = "李兴华" ;	// 定义字符串
+		int age = 30 ;				// 定义整数
+		float score = 990.356f ;	// 定义小数
+		char sex = 'M' ;			// 定义字符
+		ps.printf("姓名：%s；年龄：%d；成绩：%f；性别：%c",name,age,score,sex) ;
 		ps.close() ;
 	}
 };
